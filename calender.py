46a54929322ecb2e6bc18437c481a3a0db9c9055
Codes Priyansh <codespriyansh@gmail.com>
2024-05-10 16:39:48 +0530

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,9 @@
+#Calendar: Implement a basic calendar application that displays the current month.
+import calendar
+theyear = 2024
+themonth = 5
+cal = calendar.TextCalendar()
+cal_updated = cal.formatmonth(theyear, themonth)
+print(cal_updated) 
+
+   
